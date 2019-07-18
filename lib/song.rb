6e809baaa6774cac7f478305e3cde93b04bc1dc3
Mary Selig <seligmar@gmail.com>
2019-07-18 15:30:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -51,6 +51,7 @@ class Song
     # @artist_name = artist_name 
      new_info = filename
      first_half = new_info.split(" - ")
+     first_half[0] = 
     # second_half = first_half.split(".")
      binding.pry
      #"Taylor Swift - Blank Space.mp3" 
