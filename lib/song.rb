518a09d3a0cf4dbe917400a6888dd7daff1b918e
Mary Selig <seligmar@gmail.com>
2019-07-18 15:58:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -70,16 +70,6 @@ class Song
      @@all << song
    end 
    
-  #   describe '.new_from_filename' do
-  #   it 'initializes a song and artist_name based on the filename format' do
-  #     song = Song.new_from_filename("Thundercat - For Love I Come.mp3")
-
-  #     expect(song.name).to eq("For Love I Come")
-  #     expect(song.artist_name).to eq("Thundercat")
-  #   end
-  # end
-  
-  
   def self.destroy_all
     self.all.clear 
   end 
